a04cc121d99d6a31a6560f247c7bb2af7f26e5ca
AnonimiRML <ilayharari1234@gmail.com>
2022-05-28 12:50:31 +0100

    added some changes in index.js app that track keyword and console the catched tweets on the screen

--- index.js
+++ index.js
@@ -15,3 +15,37 @@ http.createServer(function (req, res) {
 }).listen(port);
 
 /* END SERVER */
+
+
+const config = {  
+    consumer_key: '04tA5YUNfIi73GG4wksEo1hjJ',  
+    consumer_secret: 'JSFRI0DGvIzzBXllcHY3DQu3bYWXCvJdnaIx2hopMccVgiCgr8',
+    access_token: '1515628251862949892-YkOeKzPJXUXv72qfuen36mSKLnmGlk',  
+    access_token_secret: 'H7ytmndLaNcTwXJV2NU1dfoUy3WgtFRVin6PFsz9XdiPY'
+}
+
+const Twitter = new twit(config);
+
+function tweetNow(tweetTxt) {  
+    
+    tweet = {
+        status: tweetTxt,
+    }
+    
+    Twitter.post('statuses/update', tweet, function(err, data, response) {
+        if (err) {
+            console.log('Error in Replying');
+            console.error(err);
+        } else {
+            console.log('Tweet sent successfully');
+        }
+    });
+
+}
+
+
+const stream = T.stream('statuses/filter', { track: 'כשל שוק' })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
